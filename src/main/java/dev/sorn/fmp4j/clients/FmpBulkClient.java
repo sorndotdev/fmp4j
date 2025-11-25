169278b999dc2320945736975d13a5a5f1b69fa1
milosvrabel <milos.vrabel@student.tuke.sk>
2025-11-25 20:02:29 +0100

    #82 - implemented cash flow statement bulk endpoint with tests

--- src/main/java/dev/sorn/fmp4j/clients/FmpBulkClient.java
+++ src/main/java/dev/sorn/fmp4j/clients/FmpBulkClient.java
@@ -3,8 +3,10 @@ package dev.sorn.fmp4j.clients;
 import dev.sorn.fmp4j.cfg.FmpConfig;
 import dev.sorn.fmp4j.http.FmpHttpClient;
 import dev.sorn.fmp4j.models.FmpBalanceSheetStatement;
+import dev.sorn.fmp4j.models.FmpCashFlowStatement;
 import dev.sorn.fmp4j.models.FmpCompanies;
 import dev.sorn.fmp4j.services.FmpBulkBalanceSheetStatementService;
+import dev.sorn.fmp4j.services.FmpBulkCashFlowStatementService;
 import dev.sorn.fmp4j.services.FmpBulkCompaniesService;
 import dev.sorn.fmp4j.services.FmpService;
 import dev.sorn.fmp4j.types.FmpPart;
@@ -16,10 +18,12 @@ public class FmpBulkClient {
     // Alphabetical order
     protected final FmpService<FmpCompanies[]> fmpBulkCompaniesService;
     protected final FmpService<FmpBalanceSheetStatement[]> fmpBulkBalanceSheetService;
+    protected final FmpService<FmpCashFlowStatement[]> fmpBulkCashFlowService;
 
     public FmpBulkClient(FmpConfig fmpConfig, FmpHttpClient fmpHttpClient) {
         this.fmpBulkCompaniesService = new FmpBulkCompaniesService(fmpConfig, fmpHttpClient);
         this.fmpBulkBalanceSheetService = new FmpBulkBalanceSheetStatementService(fmpConfig, fmpHttpClient);
+        this.fmpBulkCashFlowService = new FmpBulkCashFlowStatementService(fmpConfig, fmpHttpClient);
     }
 
     public synchronized FmpCompanies[] companies(FmpPart part) {
@@ -31,5 +35,12 @@ public class FmpBulkClient {
         fmpBulkBalanceSheetService.param("year", year);
         fmpBulkBalanceSheetService.param("period", period);
         return fmpBulkBalanceSheetService.download();
+    }
+
+    public synchronized FmpCashFlowStatement[] cashFlowStatements(FmpYear year, FmpPeriod period) {
+        fmpBulkCashFlowService.param("year", year);
+        fmpBulkCashFlowService.param("period", period);
+
+        return fmpBulkCashFlowService.download();
     }
 }
